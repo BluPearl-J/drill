79695f79505111eb0b0b20ec5ef60af2e77ce39b
BluPearl-J <ogheneokeno@gmail.com>
2025-08-08 14:45:24 +0100

    Create TaskSix.java

---
+++ TaskSix.java
@@ -0,0 +1,16 @@
+public class TaskSix{
+public static void main(String[] args) {
+       
+for (int i = 0; i<=10; i++) {
+if (i%4==0){
+System.out.println(i);
+System.out.println(i*i);
+System.out.println(i*i*i);
+System.out.println(i*i*i*i);
+System.out.println(i*i*i*i*i);
+}
+
+}
+
+}
+}
