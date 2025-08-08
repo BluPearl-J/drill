237c0cd00876f9b0e642c3e8092ab53e1dd23d1a
BluPearl-J <ogheneokeno@gmail.com>
2025-08-08 14:50:51 +0100

    Create TaskSeven.java

---
+++ TaskSeven.java
@@ -0,0 +1,15 @@
+public class TaskSeven{
+public static void main(String[] args) {
+       
+for (int i = 0; i<=10; i++) {
+if (i%4==0){
+int sum=(i+(i*i)+(i*i*i)+(i*i*i*i)+(i*i*i*i*i));
+System.out.println(sum);
+
+}
+
+}
+
+}
+}
+
