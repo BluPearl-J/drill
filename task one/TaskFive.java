e9bac84fd2c6fdb346c9e1ee3620d77e7d44cc2d
BluPearl-J <ogheneokeno@gmail.com>
2025-08-08 14:54:23 +0100

    Create TaskFive.java

---
+++ task one/TaskFive.java
@@ -0,0 +1,16 @@
+public class TaskFive{
+public static void main(String[] args) {
+       
+for (int i = 0; i<=10; i++) {
+if (i%4==0){
+System.out.print(i);
+System.out.print(i);
+System.out.print(i);
+System.out.print(i);
+System.out.print(i);
+}
+
+}
+
+}
+}
